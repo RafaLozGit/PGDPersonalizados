6e986b02f8c907b5c511506008181c75bbd89e6a
Rafael <rafael@email.com>
2026-03-18 21:39:42 -0300

    Configura admin com list_display, search_fields e list_filter

--- loja/admin.py
+++ loja/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Cliente, Produto, Pedido
 
-admin.site.register(Cliente)
-admin.site.register(Produto)
-admin.site.register(Pedido)
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'telefone')
+    search_fields = ('nome',)
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'preco')
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cliente', 'produto', 'quantidade', 'total')
+    list_filter = ('cliente',)
