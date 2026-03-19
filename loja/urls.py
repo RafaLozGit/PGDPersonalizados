907f19874bd886fd3c58ec1151e332306027793b
Rafael <rafael@email.com>
2026-03-18 22:07:49 -0300

    Atualiza Home moderna, CSS e URLs integradas

--- loja/urls.py
+++ loja/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.home, name='home'),
-    path('cliente/', views.cadastrar_cliente, name='cliente'),
-    path('produto/', views.cadastrar_produto, name='produto'),
-    path('pedido/', views.cadastrar_pedido, name='pedido'),
+    path('cliente/', views.cadastrar_cliente, name='cliente_list'),
+    path('produto/', views.cadastrar_produto, name='produto_list'),
+    path('pedido/', views.cadastrar_pedido, name='pedido_list'),
     path('excluir/<int:id>/', views.excluir_pedido, name='excluir'),
 ]
